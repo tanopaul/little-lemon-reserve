cb10942d14a11f7ed9bbb34a0090be459ccbd540
Tano Paul <tanopaul38@gmail.com>
2023-05-04 18:57:31 -0400

    styled nav

--- src/nav.js
+++ src/nav.js
@@ -4,12 +4,13 @@ function Nav() {
     return (
         <div className="nav">
             <img src={logo} alt="logo" className='logo'/>
-            <ul>
-                <li>Our Menu</li>
-                <li>Reservations</li>
-                <li>Order Online</li>
-                <li>Our Story</li>
-                <li>Careers</li>
+            <h3 className='logo-title'>Little Lemon</h3>
+            <ul className='nav-menu'>
+                <li><a href='.nav'>Our Menu</a></li>
+                <li><a href='.nav'>Reservations</a></li>
+                <li><a href='.nav'>Order Online</a></li>
+                <li><a href='.nav'>Our Story</a></li>
+                <li><a href='.nav'>Careers</a></li>
             </ul>
             <div className='nav-locations'>
                 <h6>VIEW ALL LOCATIONS</h6>
