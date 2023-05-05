a4fb4626a39556b72b74f6cbd807af994b847589
Tano Paul <tanopaul38@gmail.com>
2023-05-05 12:08:10 -0400

    styled reservations section

--- src/reservations.js
+++ src/reservations.js
@@ -1,8 +1,23 @@
+import instagram from './assets/icons8-linkedin-50.png'
+
 function Reserve() {
     return (
         <div>
-            <h1 className="yellow">hello world</h1>
-            <h1 className="red">hello world</h1>
+            <section className="reservations-sec">
+                <div className='reservations-div'>
+                    <h3>Healthy cookin' Healthy livin'</h3>
+                    <h1>RESERVATIONS</h1>
+                </div>
+                </section>
+            <footer className="footer">
+                <div className="footer-div-social">
+                    <img src={instagram} alt="instagram-icon"/>
+                    <img src={instagram} alt="instagram-icon"/>
+                    <img src={instagram} alt="instagram-icon"/>
+                    <img src={instagram} alt="instagram-icon"/>
+                    <img src={instagram} alt="instagram-icon"/>
+                </div>
+            </footer>
         </div>
     )
 }
