c3d20073bd946fd24a7a20bbcee364cbd9aa981e
Tano Paul <tanopaul38@gmail.com>
2023-05-05 16:43:31 -0400

    added more styles to reservations

--- src/reservations.js
+++ src/reservations.js
@@ -8,7 +8,33 @@ function Reserve() {
                     <h3>Healthy cookin' Healthy livin'</h3>
                     <h1>RESERVATIONS</h1>
                 </div>
-                </section>
+
+                <form>
+                    <div className='input-div'>
+                        <div className='location-div'>
+                            <label for= "location">Location</label>
+                            <input type='text' id='location'></input>
+                        </div>
+                        <div className='location-div'>
+                            <label for= "location1">Date</label>
+                            <input type='date' id='location1'></input>
+                        </div>
+                        <div className='location-div'>
+                            <label for= "location2">Time</label>
+                            <input type='select' id='location2'></input>
+                        </div>
+                        <div className='location-div'>
+                            <label for= "location3">Party Size</label>
+                            <input type='text' id='location3'></input>
+                        </div>
+                    </div>
+                    <input type='submit' value='Check Availability' className='submit'/>
+                </form>
+                <div>
+                    <h6>Disclaimer: <a href=''>Privacy Policy</a> <a href=''>Terms of Use</a></h6>
+                    <p>Powered by:</p>
+                </div>
+                    </section>
             <footer className="footer">
                 <div className="footer-div-social">
                     <img src={instagram} alt="instagram-icon"/>
